c1ce97184ec8e1d9f3af5e59d8069be6c6863537
kameleonus <kamil10.00@o2.pl>
2023-08-01 21:58:20 +0200

    Setting up

---
+++ src/main/java/pl/kamil/skutery/SkuteryApplication.java
@@ -0,0 +1,13 @@
+package pl.kamil.skutery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkuteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkuteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kamil/skutery/SkuteryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kamil.skutery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkuteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
